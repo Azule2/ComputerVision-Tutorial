f0910d27d1666b449a187957dcf30060a60679a3
Azule2 <69428406+Azule2@users.noreply.github.com>
2022-08-07 20:55:44 +0300

    Update Chapter-1.py

--- Chapter-1.py
+++ Chapter-1.py
@@ -3,6 +3,27 @@ import cv2
 #-----Chapter 1 -----(Read Image)
 img = cv2.imread('test.png')
 
-
+#Head Name Application and show img
 cv2.imshow('Show Img', img)
 cv2.waitKey(0)
+#-----Chapter 1 -----(Video Camera)
+
+#VideoCapture (0,1 Camera)
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+cap.set(10,100)
+
+while True:
+ 	success, img = cap.read()
+ 	cv2.imshow('Video', img)
+  #press q (quit Application)
+ 	if cv2.waitKey(1) & 0xFF == ord('q'):
+    
+	 	break 
+#-----Chapter 1 -----(Read Video)
+cap = cv2.VideoCapture('test.mp4')
+while True:
+ 	success, img = cap.read()
+ 	cv2.imshow('Video', img)
+ 	if cv2.waitKey(1) & 0xFF == ord('q'):
